f346de5aaacc813087d546af4f37cedc5b6881c0
Ye Yint Naung <yeyintnaung@ritz.com.mm>
2020-03-23 15:51:03 +0630

    to be continue

--- resources/js/app.js
+++ resources/js/app.js
@@ -35,6 +35,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 Vue.component('Piechart', require('./components/Piechart.vue').default);
 Vue.component('progressbar', require('./components/progressbar.vue').default);
+Vue.component('Realtimenoti', require('./components/realtimenoti.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
